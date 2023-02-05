770f2adb4980edf8786d78f71f0e4cee4252abbe
Dr. Sven-Eric Molzahn <s.molzahn@me.com>
2023-02-05 13:36:54 +0100

    added migrations

--- prisma/migrations/20210202045146_init/migration.sql
+++ prisma/migrations/20210202045146_init/migration.sql
@@ -7,3 +7,27 @@ CREATE TABLE "Todo" (
 
     PRIMARY KEY ("id")
 );
+
+CREATE TABLE Room (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomNumber VARCHAR(255),
+  capacity INT,
+  amenities VARCHAR(255)
+);
+
+CREATE TABLE Booking (
+  id VARCHAR(255) PRIMARY KEY,
+  roomId INT,
+  startDate VARCHAR(255),
+  endDate VARCHAR(255),
+  customerId VARCHAR(255),
+  FOREIGN KEY (roomId) REFERENCES Room(id),
+  FOREIGN KEY (customerId) REFERENCES Customer(id)
+);
+
+CREATE TABLE Customer (
+  id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  phoneNumber VARCHAR(255)
+);
